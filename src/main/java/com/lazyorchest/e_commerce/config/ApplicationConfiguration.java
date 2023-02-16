bdf4f852b6c20af765e233b4970913a9b59b0a05
Fazri Ridwan <fazriridwan19@gmail.com>
2023-02-16 10:02:23 +0700

    Add command line runner bean and cors policy setting

--- src/main/java/com/lazyorchest/e_commerce/config/ApplicationConfiguration.java
+++ src/main/java/com/lazyorchest/e_commerce/config/ApplicationConfiguration.java
@@ -1,9 +1,16 @@
 package com.lazyorchest.e_commerce.config;
 
+import com.lazyorchest.e_commerce.dto.ProductRequest;
+import com.lazyorchest.e_commerce.dto.RegisterRequest;
 import com.lazyorchest.e_commerce.repositories.UserRepo;
+import com.lazyorchest.e_commerce.services.AuthService;
+import com.lazyorchest.e_commerce.services.ProductService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNullApi;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,6 +20,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -41,6 +50,37 @@ public class ApplicationConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    CommandLineRunner run(AuthService authService, ProductService productService) {
+        return args -> {
+            authService.register(
+                    new RegisterRequest("Fazri", "fazri", "fazri")
+            );
+            productService.createProduct(
+                    new ProductRequest("Product 1", 200000.0, 2, "category1")
+            );
+            productService.createProduct(
+                    new ProductRequest("Product 2", 75000.0, 1, "category1")
+            );
+            productService.createProduct(
+                    new ProductRequest("Product 3", 50000.0, 1, "category2")
+            );
+            productService.createProduct(
+                    new ProductRequest("Product 4", 90000.0, 1, "category3")
+            );
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOriginPatterns("*").allowCredentials(true);
+            }
+        };
     }
 
 /*
